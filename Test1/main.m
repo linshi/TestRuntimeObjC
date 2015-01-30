c5ce45a1e49c03f8b12d2dc6b4bbbc9d96c9b9e2
Lin <forrest.lin@sap.com>
2015-01-30 15:18:16 +0800

    init

--- Test1/main.m
+++ Test1/main.m
@@ -8,10 +8,48 @@
 
 #import <Foundation/Foundation.h>
 
+// ref to
+// http://cocoasamurai.blogspot.sg/2010/01/understanding-objective-c-runtime.html
+
+@interface MyObject : NSObject
+
+- (instancetype)init;
+
+@end
+
+@implementation MyObject
+
+- (instancetype)init{
+    self = [super init];
+    
+    return self;
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        id obj1 = [NSMutableArray alloc];
+        id obj2 = [[NSMutableArray alloc] init];
+        
+        id obj3 = [NSArray alloc];
+        id obj4 = [[NSArray alloc] initWithObjects:@"Hello",nil];
+        
+        NSLog(@"obj1 class is %@",NSStringFromClass([obj1 class]));
+        NSLog(@"obj2 class is %@",NSStringFromClass([obj2 class]));
+        
+        NSLog(@"obj3 class is %@",NSStringFromClass([obj3 class]));
+        NSLog(@"obj4 class is %@",NSStringFromClass([obj4 class]));
+        
+        id obj5 = [MyObject alloc];
+        id obj6 = [[MyObject alloc] init];
+        
+        NSLog(@"obj5 class is %@",NSStringFromClass([obj5 class]));
+        NSLog(@"obj6 class is %@",NSStringFromClass([obj6 class]));
+
+        
     }
     return 0;
 }
