c93b4dc1b6364175d7cf8ce9b651d22084955ea5
Lin <forrest.lin@sap.com>
2015-01-30 14:38:37 +0800

    Initial Commit

---
+++ Test1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test1
+//
+//  Created by Lin, Forrest on 1/30/15.
+//  Copyright (c) 2015 Lin, Forrest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
